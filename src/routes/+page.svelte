a5e64e115fc8f28c95376fc28389880dc340e2fd
Kresimir Delija <kresimir.delija@wearenotch.com>
2025-04-15 09:00:20 +0200

    Update +page.svelte
    Basic binding and function call

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+    let name = $state('world')
+
+    let location = $state('Notch')
+
+	let currentCount = $state(0);
+	function add() {
+		currentCount++;
+	}
+</script>
+
+<h1>Welcome to first Notch conference</h1>
+<h1>Svelte demo 01</h1>
+<br/>
+<h2>Osnovni binding</h2>
+<p>Hello {name}</p>
+<br/>
+<h2>Reactive binding</h2>
+<input type="text" bind:value={location}>
+<p>Hello {name} from {location}</p>
+<br/>
+<h2>Poziv funkcije</h2>
+<p>Clicked {currentCount} times</p>
+<button onclick={add}>Dodaj</button>
